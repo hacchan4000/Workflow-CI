b919fdc30c35a92cc90f9dcb96a2c62352a8cf69
Chandra <adityanugraha7251@gmail.com>
2025-12-01 16:30:41 +0800

    gabisa nih

--- Project/modelling.py
+++ Project/modelling.py
@@ -3,13 +3,13 @@ import numpy as np
 import math
 import mlflow
 import mlflow.sklearn
+import os
 
 from sklearn.svm import SVR
 from sklearn.metrics import mean_squared_error
 
 df = pd.read_csv("aapl.us.txt_preprocessing.csv")
 dataset = df["Close_norm"].values.reshape(-1, 1)
-
 
 def create_window(data, window_size=60):
     X, y = [], []
@@ -18,7 +18,6 @@ def create_window(data, window_size=60):
         y.append(data[i, 0])
     return np.array(X), np.array(y)
 
-# ==== Split Train/Test ====
 training_data_len = math.ceil(len(dataset) * 0.8)
 train_data = dataset[:training_data_len]
 test_data  = dataset[training_data_len - 60:]
@@ -26,30 +25,31 @@ test_data  = dataset[training_data_len - 60:]
 X_train, y_train = create_window(train_data, 60)
 X_test,  y_test  = create_window(test_data, 60)
 
-
-
-import os
+# Tracking URI
 mlflow.set_tracking_uri(f"file://{os.getcwd()}/mlruns")
 mlflow.set_experiment("ci_retrain_model")
 
+# FIX: MLflow Project already creates a run, so don't create another
+import sys
 
-with mlflow.start_run():
-    model = SVR(kernel="rbf", C=100, gamma=0.1)
-    model.fit(X_train, y_train)
-    
-    input_example = X_test[:1]   # one row example
-    signature = mlflow.models.signature.infer_signature(X_train, model.predict(X_train))
+run_id = sys.argv[1]
+mlflow.start_run(run_id=run_id)
 
 
-    predictions = model.predict(X_test)
-    rmse = np.sqrt(mean_squared_error(y_test, predictions))
-    print("RMSE:", rmse)
+model = SVR(kernel="rbf", C=100, gamma=0.1)
+model.fit(X_train, y_train)
 
-    # Log metrics and model
-    mlflow.log_metric("RMSE", rmse)
-    mlflow.sklearn.log_model(model, 
-                             name="model",
-                             input_example=input_example,
-                             signature=signature
-                             )
+predictions = model.predict(X_test)
+rmse = np.sqrt(mean_squared_error(y_test, predictions))
 
+mlflow.log_metric("RMSE", rmse)
+
+input_example = X_test[:1]
+signature = mlflow.models.signature.infer_signature(X_train, model.predict(X_train))
+
+mlflow.sklearn.log_model(
+    model,
+    "model",
+    input_example=input_example,
+    signature=signature
+)
